792ead41e23499fec0d262e51b73ed79e8b49721
Xuan Tran <xuandungdkl@gmail.com>
2024-09-20 15:10:37 +0200

    Add project files.

---
+++ Apu Real Estate (ARE)/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Apu_Real_Estate__ARE_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            groupBox4 = new GroupBox();
+            groupBox5 = new GroupBox();
+            groupBox6 = new GroupBox();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(32, 25);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 100);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(283, 25);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(200, 100);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Location = new Point(32, 154);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(467, 161);
+            groupBox3.TabIndex = 1;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "groupBox3";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Location = new Point(48, 183);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(200, 100);
+            groupBox4.TabIndex = 1;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "groupBox4";
+            // 
+            // groupBox5
+            // 
+            groupBox5.Location = new Point(283, 183);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(200, 100);
+            groupBox5.TabIndex = 1;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "groupBox5";
+            // 
+            // groupBox6
+            // 
+            groupBox6.Location = new Point(32, 350);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(783, 214);
+            groupBox6.TabIndex = 1;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "groupBox6";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1402, 597);
+            Controls.Add(groupBox6);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox3);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private GroupBox groupBox4;
+        private GroupBox groupBox5;
+        private GroupBox groupBox6;
+    }
+}
---
+++ Apu Real Estate (ARE)/Form1.cs
@@ -0,0 +1,10 @@
+namespace Apu_Real_Estate__ARE_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
