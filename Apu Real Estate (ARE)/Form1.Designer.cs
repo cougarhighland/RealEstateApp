8631c363cb7c5f533a23ecb5271a0b9f44daa876
Xuan Tran <xuandungdkl@gmail.com>
2024-09-26 13:06:02 +0200

    update view

--- Apu Real Estate (ARE)/Form1.Designer.cs
+++ Apu Real Estate (ARE)/Form1.Designer.cs
@@ -38,69 +38,82 @@
             // 
             // groupBox1
             // 
-            groupBox1.Location = new Point(32, 25);
+            groupBox1.Location = new Point(46, 42);
+            groupBox1.Margin = new Padding(4, 5, 4, 5);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(200, 100);
+            groupBox1.Padding = new Padding(4, 5, 4, 5);
+            groupBox1.Size = new Size(286, 167);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
+            groupBox1.Text = "Estate-common data";
             // 
             // groupBox2
             // 
-            groupBox2.Location = new Point(283, 25);
+            groupBox2.Location = new Point(404, 42);
+            groupBox2.Margin = new Padding(4, 5, 4, 5);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(200, 100);
+            groupBox2.Padding = new Padding(4, 5, 4, 5);
+            groupBox2.Size = new Size(286, 167);
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
-            groupBox2.Text = "groupBox2";
+            groupBox2.Text = "Address";
             // 
             // groupBox3
             // 
-            groupBox3.Location = new Point(32, 154);
+            groupBox3.Location = new Point(46, 257);
+            groupBox3.Margin = new Padding(4, 5, 4, 5);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(467, 161);
+            groupBox3.Padding = new Padding(4, 5, 4, 5);
+            groupBox3.Size = new Size(667, 268);
             groupBox3.TabIndex = 1;
             groupBox3.TabStop = false;
-            groupBox3.Text = "groupBox3";
+            groupBox3.Text = "Specificed Data";
             // 
             // groupBox4
             // 
-            groupBox4.Location = new Point(48, 183);
+            groupBox4.Location = new Point(69, 305);
+            groupBox4.Margin = new Padding(4, 5, 4, 5);
             groupBox4.Name = "groupBox4";
-            groupBox4.Size = new Size(200, 100);
+            groupBox4.Padding = new Padding(4, 5, 4, 5);
+            groupBox4.Size = new Size(286, 167);
             groupBox4.TabIndex = 1;
             groupBox4.TabStop = false;
-            groupBox4.Text = "groupBox4";
+            groupBox4.Text = "Category-specific";
             // 
             // groupBox5
             // 
-            groupBox5.Location = new Point(283, 183);
+            groupBox5.Location = new Point(404, 305);
+            groupBox5.Margin = new Padding(4, 5, 4, 5);
             groupBox5.Name = "groupBox5";
-            groupBox5.Size = new Size(200, 100);
+            groupBox5.Padding = new Padding(4, 5, 4, 5);
+            groupBox5.Size = new Size(286, 167);
             groupBox5.TabIndex = 1;
             groupBox5.TabStop = false;
-            groupBox5.Text = "groupBox5";
+            groupBox5.Text = "Object-specific";
             // 
             // groupBox6
             // 
-            groupBox6.Location = new Point(32, 350);
+            groupBox6.Location = new Point(46, 583);
+            groupBox6.Margin = new Padding(4, 5, 4, 5);
             groupBox6.Name = "groupBox6";
-            groupBox6.Size = new Size(783, 214);
+            groupBox6.Padding = new Padding(4, 5, 4, 5);
+            groupBox6.Size = new Size(1119, 357);
             groupBox6.TabIndex = 1;
             groupBox6.TabStop = false;
-            groupBox6.Text = "groupBox6";
+            groupBox6.Text = "List of Estate";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1402, 597);
+            ClientSize = new Size(1924, 995);
             Controls.Add(groupBox6);
             Controls.Add(groupBox2);
             Controls.Add(groupBox4);
             Controls.Add(groupBox5);
             Controls.Add(groupBox1);
             Controls.Add(groupBox3);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
