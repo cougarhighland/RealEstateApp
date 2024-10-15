214dc0b9e5389ce74f7097600069c54924ac4a48
Xuan Tran <xuandungdkl@gmail.com>
2024-10-15 14:03:22 +0200

    some lib

---
+++ UtilitiesLib/StringConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace UtilitiesLib
+{
+    public class StringConverter
+    {
+        public static bool StringToInt(string str)
+        {
+            return int.TryParse(str, out var result);
+        }
+    }
+}
